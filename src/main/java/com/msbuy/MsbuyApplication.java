ee70e210aff0f8e31fdd18f93d662df7458c4c94
Natan <natan.nbg@gmail.com>
2024-02-21 18:38:51 -0300

    creation of the project

---
+++ src/main/java/com/msbuy/MsbuyApplication.java
@@ -0,0 +1,13 @@
+package com.msbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msbuy/MsbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
